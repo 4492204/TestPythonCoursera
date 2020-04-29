9ede4026ea9fd63f5627d0d320875d5ac2061fc8
Mikhail L <4492204@gmail.com>
2020-04-29 12:30:13 +0300

    Show complex digit

--- Test.py
+++ Test.py
@@ -2,11 +2,9 @@ class Complex:
     def __init__(self, re=0, im=0):
         self.re = re
         self.im = im
-
-
+    def __str__(self):
+        return str(self.re) + '+' +str(self.im) + 'i'
 a = Complex(1, 2)
 b = Complex(3)
 c = Complex()
-print(a.re, a.im)
-print(b.re, b.im)
-print(c.re, c.im)
+print(b)
